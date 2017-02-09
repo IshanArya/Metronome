93049b1ad3c05e06392991d0ede66cc8cefc6feb
IshanArya <ishan.arya130@gmail.com>
2017-02-09 01:12:04 -0500

    Got play/stop button in slider. Changed from svg to icons (why didn't I know about this before?)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-	var toggleSound = document.getElementById('toggleSound');
-	var iconOfToggleSound = document.querySelector('#toggleSound > svg > use');
+	var toggleSound = $('<button id="toggleSound"><i class="icon-play"></i></svg></button>');
+	var iconOfToggleSound = $('#toggleSound > i');
 	var slider = $('#slider');
 
 	var intervalId;
@@ -13,22 +13,36 @@ window.onload = function() {
 	function playSound(sound) {
 		sound.play();
 	}
+
 	slider.roundSlider({
 		max: 360,
 		min: 20,
 		value: 120,
-		startAngle: 120,
-		
-
-	});
-	toggleSound.addEventListener('click', function() {
-		if(iconOfToggleSound.getAttribute("xlink:href") === "#play") {
-			iconOfToggleSound.setAttribute("xlink:href", "#pause");
-			intervalId = setInterval(playSound, 500, marimba_1);
-		} else {
-			iconOfToggleSound.setAttribute("xlink:href", "#play");
-			clearInterval(intervalId);
+		radius: 360,
+		create: function () {
+		  	this.innerBlock.append(toggleSound);
+	  		toggleSound.on('click', function() {
+	  	  		if(iconOfToggleSound.hasClass("icon-play")) {
+	  					intervalId = setInterval(playSound, 500, marimba_1);
+	  	  		} else {
+	  	  			clearInterval(intervalId);
+	  	  		}
+	  	  		iconOfToggleSound.toggleClass("icon-play icon-stop");
+	  	  	});
+		},
+		start: function() {
+			var that = this;
+		  	var btn1 = $("<button id='sub'>-</button>");
+		    var btn2 = $("<button id='add'>+</button>");
+		  	this.innerBlock.append(btn1);
+		    this.innerBlock.append(btn2);
+		    btn1.click(function() {
+		    	that.setValue(that.options.value - 1);
+		    });
+		    btn2.click(function() {
+		    	that.setValue(that.options.value + 1);
+		    });
 		}
-		
 	});
+
 }
